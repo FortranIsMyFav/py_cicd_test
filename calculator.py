f0e333d1ed8ac233d218d3f29e66aa96514d6a83
Shane <shane.c.moran@gmail.com>
2021-12-03 15:38:30 -0700

    check if style fails

--- calculator.py
+++ calculator.py
@@ -2,7 +2,6 @@
 Calculator library containing basic math operations.
 """
 
-
 def add(first_term, second_term):
     return first_term + second_term
 
@@ -17,8 +16,6 @@ def multiply(first_term, second_term):
 
 def divide(first_term, second_term):
     return first_term / second_term
-
-
 
 
 def pow(first_term, second_term):
